7307cd4f2445eb5ffd1fea61a90a9c7cf548a64e
doug0102 <shea.wow@hotmail.com>
2026-07-29 12:49:51 -0400

    Auth.cs

---
+++ Auth.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+
+namespace GCStats
+{
+    class Auth
+    {
+        public GraphServiceClient GraphAuth(ILogger log)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+           .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+           .AddEnvironmentVariables()
+           .Build();
+
+            var scopes = new string[] { "https://graph.microsoft.com/.default" };
+            var keyVaultUrl = config["keyVaultUrl"];
+            var keyname = config["keyname"];
+            var tenantId = config["tenantid"];
+            var clientId = config["clientid"];
+
+            SecretClientOptions options = new SecretClientOptions()
+            {
+                Retry =
+                {
+                    Delay= TimeSpan.FromSeconds(2),
+                    MaxDelay = TimeSpan.FromSeconds(16),
+                    MaxRetries = 5,
+                    Mode = RetryMode.Exponential
+                 }
+            };
+
+            var client = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential(), options);
+
+            KeyVaultSecret secret = client.GetSecret(keyname);
+
+            var optionsToken = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            var clientSecretCredential = new ClientSecretCredential(tenantId, clientId, secret.Value, optionsToken);
+
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+            return graphClient;
+        }
+    }
+}
