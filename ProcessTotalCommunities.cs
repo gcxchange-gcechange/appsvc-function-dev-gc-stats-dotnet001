d0b360699ae985dfaed3d1e240a41d07736ee736
doug0102 <shea.wow@hotmail.com>
2026-09-14 12:53:59 -0400

    refactor ProcessTotalCommunities.cs from JSON to Parquet

--- ProcessTotalCommunities.cs
+++ ProcessTotalCommunities.cs
@@ -1,5 +1,7 @@
 ﻿using Azure.Identity;
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Azure.Storage.Sas;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
@@ -29,118 +31,48 @@ namespace GCStats
             {
                 var storageAccountUrl = Globals.GetAppSetting("storageAccountUrl", _logger, _config);
                 var isLocal = Globals.GetAppSetting("isLocal", _logger, _config, false);
+                var credential = isLocal == "true" ? new AzureCliCredential() : (Azure.Core.TokenCredential)new DefaultAzureCredential();
 
-                var blobServiceClient = new BlobServiceClient(new Uri(storageAccountUrl), isLocal == "true" ? new AzureCliCredential() : new DefaultAzureCredential());
-                var containerClient = blobServiceClient.GetBlobContainerClient(Communities.TotalCommunitiesContainerName);
-                var blobClient = containerClient.GetBlobClient(blobName);
+                var blobServiceClient = new BlobServiceClient(new Uri(storageAccountUrl), credential);
 
-                var response = await blobClient.DownloadContentAsync();
-                var communities = JsonSerializer.Deserialize<List<CommunityRecord>>(response.Value.Content.ToString(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                var date = blobName.Replace("communities-", string.Empty).Replace(".parquet", string.Empty);
+                var ownersBlobName = $"community-owners-{date}.parquet";
+                var membersBlobName = $"community-members-{date}.parquet";
 
-                if (communities != null && communities.Count > 0)
-                {
-                    _logger.LogInformation("Processing {count} communities from blob: {blobName}", communities.Count, blobName);
+                var communitiesContainerClient = blobServiceClient.GetBlobContainerClient(Communities.TotalCommunitiesContainerName);
+                var ownersContainerClient = blobServiceClient.GetBlobContainerClient(Communities.CommunityOwnersContainerName);
+                var membersContainerClient = blobServiceClient.GetBlobContainerClient(Communities.CommunityMembersContainerName);
 
-                    var communityDataTable = new DataTable();
-                    communityDataTable.Columns.Add("Id", typeof(string));
-                    communityDataTable.Columns.Add("DisplayName", typeof(string));
-                    communityDataTable.Columns.Add("SensitivityLabelId", typeof(string));
-                    communityDataTable.Columns.Add("CreationDate", typeof(DateTime));
-                    communityDataTable.Columns.Add("LastActivityDate", typeof(DateTime));
-                    communityDataTable.Columns.Add("SnapshotDate", typeof(DateTime));
+                var communitiesBlobClient = communitiesContainerClient.GetBlobClient(blobName);
+                var ownersBlobClient = ownersContainerClient.GetBlobClient(ownersBlobName);
+                var membersBlobClient = membersContainerClient.GetBlobClient(membersBlobName);
 
-                    var ownerDataTable = new DataTable();
-                    ownerDataTable.Columns.Add("Id", typeof(string));
-                    ownerDataTable.Columns.Add("CommunityId", typeof(string));
-                    ownerDataTable.Columns.Add("SnapshotDate", typeof(DateTime));
+                if (!await communitiesBlobClient.ExistsAsync())
+                    throw new FileNotFoundException($"Blob {blobName} not found in container {Communities.TotalCommunitiesContainerName}");
 
-                    var memberDataTable = new DataTable();
-                    memberDataTable.Columns.Add("Id", typeof(string));
-                    memberDataTable.Columns.Add("CommunityId", typeof(string));
-                    memberDataTable.Columns.Add("SnapshotDate", typeof(DateTime));
+                if (!await ownersBlobClient.ExistsAsync())
+                    throw new FileNotFoundException($"Blob {ownersBlobName} not found in container {Communities.CommunityOwnersContainerName}");
 
-                    var snapshotDate = Globals.GetDateFromBlob(blobName, _logger);
+                if (!await membersBlobClient.ExistsAsync())
+                    throw new FileNotFoundException($"Blob {membersBlobName} not found in container {Communities.CommunityMembersContainerName}");
 
-                    foreach (var community in communities)
+                var delegationKey = await blobServiceClient.GetUserDelegationKeyAsync(
+                    new BlobGetUserDelegationKeyOptions(DateTimeOffset.UtcNow.AddMinutes(15))
                     {
-                        communityDataTable.Rows.Add(community.Id, community.DisplayName, community.SensitivityLabel.Id, community.CreationDate.UtcDateTime, community.LastActivityDate.UtcDateTime, snapshotDate);
-
-                        foreach (var owner in community.OwnerList)
-                        {
-                            ownerDataTable.Rows.Add(owner.Id, community.Id, snapshotDate);
-                        }
-
-                        foreach (var member in community.MemberList)
-                        {
-                            memberDataTable.Rows.Add(member.Id, community.Id, snapshotDate);
-                        }
+                        StartsOn = DateTimeOffset.UtcNow.AddMinutes(-5)
                     }
+                );
 
-                    using var sqlConnection = await Auth.GetSqlConnection(_logger, _config);
-                    using var transaction = sqlConnection.BeginTransaction();
+                using var sqlConnection = await Auth.GetSqlConnection(_logger, _config);
 
-                    try
-                    {
-                        var bulkCopyOptions = SqlBulkCopyOptions.TableLock;
+                await CopyParquetIntoTableAsync(sqlConnection, "dbo.TotalCommunities", communitiesContainerClient, communitiesBlobClient, delegationKey.Value, blobServiceClient.AccountName);
+                _logger.LogInformation("Successfully uploaded communities from {blobName} to dbo.TotalCommunities", blobName);
 
-                        using (var bulkCopy = new SqlBulkCopy(sqlConnection, bulkCopyOptions, transaction))
-                        {
-                            bulkCopy.DestinationTableName = "dbo.TotalCommunities";
-                            bulkCopy.BatchSize = 50000;
-                            bulkCopy.BulkCopyTimeout = 0;
+                await CopyParquetIntoTableAsync(sqlConnection, "dbo.CommunityOwners", ownersContainerClient, ownersBlobClient, delegationKey.Value, blobServiceClient.AccountName);
+                _logger.LogInformation("Successfully uploaded owners from {blobName} to dbo.CommunityOwners", ownersBlobName);
 
-                            bulkCopy.ColumnMappings.Add("Id", "Id");
-                            bulkCopy.ColumnMappings.Add("DisplayName", "DisplayName");
-                            bulkCopy.ColumnMappings.Add("SensitivityLabelId", "SensitivityLabelId");
-                            bulkCopy.ColumnMappings.Add("CreationDate", "CreationDate");
-                            bulkCopy.ColumnMappings.Add("LastActivityDate", "LastActivityDate");
-                            bulkCopy.ColumnMappings.Add("SnapshotDate", "SnapshotDate");
-
-                            await bulkCopy.WriteToServerAsync(communityDataTable);
-                            _logger.LogInformation("Successfully uploaded {count} communities to dbo.TotalCommunities", communities.Count);
-                        }
-
-                        using (var bulkCopy = new SqlBulkCopy(sqlConnection, bulkCopyOptions, transaction))
-                        {
-                            bulkCopy.DestinationTableName = "dbo.CommunityOwners";
-                            bulkCopy.BatchSize = 50000;
-                            bulkCopy.BulkCopyTimeout = 0;
-
-                            bulkCopy.ColumnMappings.Add("Id", "UserId");
-                            bulkCopy.ColumnMappings.Add("CommunityId", "CommunityId");
-                            bulkCopy.ColumnMappings.Add("SnapshotDate", "SnapshotDate");
-
-                            await bulkCopy.WriteToServerAsync(ownerDataTable);
-                            _logger.LogInformation("Successfully uploaded {count} owners to dbo.CommunityOwners", ownerDataTable.Rows.Count);
-                        }
-
-                        using (var bulkCopy = new SqlBulkCopy(sqlConnection, bulkCopyOptions, transaction))
-                        {
-                            bulkCopy.DestinationTableName = "dbo.CommunityMembers";
-                            bulkCopy.BatchSize = 50000;
-                            bulkCopy.BulkCopyTimeout = 0;
-
-                            bulkCopy.ColumnMappings.Add("Id", "UserId");
-                            bulkCopy.ColumnMappings.Add("CommunityId", "CommunityId");
-                            bulkCopy.ColumnMappings.Add("SnapshotDate", "SnapshotDate");
-
-                            await bulkCopy.WriteToServerAsync(memberDataTable);
-                            _logger.LogInformation("Successfully uploaded {count} members to dbo.CommunityMembers", memberDataTable.Rows.Count);
-                        }
-
-                        transaction.Commit();
-                    }
-                    catch
-                    {
-                        // If anything fails we rollback the entire transaction
-                        transaction.Rollback();
-                        throw;
-                    }
-                }
-                else
-                {
-                    throw new DataException("No communities to upload");
-                }
+                await CopyParquetIntoTableAsync(sqlConnection, "dbo.CommunityMembers", membersContainerClient, membersBlobClient, delegationKey.Value, blobServiceClient.AccountName);
+                _logger.LogInformation("Successfully uploaded members from {blobName} to dbo.CommunityMembers", membersBlobName);
             }
             catch (Exception ex)
             {
@@ -149,6 +81,39 @@ namespace GCStats
             }
 
             _logger.LogInformation("Finished processing {blobName}", blobName);
+        }
+
+        private static async Task CopyParquetIntoTableAsync(SqlConnection sqlConnection, string destinationTable, BlobContainerClient containerClient, BlobClient blobClient, UserDelegationKey delegationKey, string accountName)
+        {
+            var sasBuilder = new BlobSasBuilder
+            {
+                BlobContainerName = containerClient.Name,
+                BlobName = blobClient.Name,
+                Resource = "b",
+                StartsOn = DateTimeOffset.UtcNow.AddMinutes(-5),
+                ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(15)
+            };
+            sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+            var sasToken = sasBuilder.ToSasQueryParameters(delegationKey, accountName).ToString();
+
+            var blobUrl = blobClient.Uri.ToString().Replace("'", "''");
+            var sasSecret = sasToken.Replace("'", "''");
+
+            using var copyCmd = new SqlCommand(
+                $"""
+                 COPY INTO {destinationTable}
+                 FROM '{blobUrl}'
+                 WITH (
+                     FILE_TYPE = 'PARQUET',
+                     CREDENTIAL = (IDENTITY = 'Shared Access Signature', SECRET = '{sasSecret}')
+                 );
+                 """,
+                sqlConnection
+            );
+
+            copyCmd.CommandTimeout = 0;
+            await copyCmd.ExecuteNonQueryAsync();
         }
     }
 }
