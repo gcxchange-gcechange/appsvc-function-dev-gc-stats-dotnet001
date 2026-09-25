e97f1de65dcfed3aab16f46ccb82a75ac02c4353
Shea <shea.wow@hotmail.com>
2026-09-25 12:36:31 -0400

    Added more fields to the schema for Users.cs - UserPrincipalName - DisplayName - UserType - AccountEnabled - PreferredLanguage - CreatedDateTime - LastPasswordChangeDateTime - ExternalUserState

--- Users.cs
+++ Users.cs
@@ -27,9 +27,19 @@ namespace GCStats
                 using var blobStream = await blobClient.OpenWriteAsync(overwrite: true);
 
                 var idField = new DataField<string>("Id");
-                var mailField = new DataField<string>("Mail");
+                var mailField = new DataField<string?>("Mail");
+                var upnField = new DataField<string?>("UserPrincipalName");
+                var displayNameField = new DataField<string?>("DisplayName");
+                var userTypeField = new DataField<string?>("UserType");
+                var accountEnabledField = new DataField<bool?>("AccountEnabled");
+                var preferredLanguageField = new DataField<string?>("PreferredLanguage");
+                var createdDateTimeField = new DataField<DateTime?>("CreatedDateTime");
+                var lastPasswordChangeField = new DataField<DateTime?>("LastPasswordChangeDateTime");
+                var externalUserStateField = new DataField<string?>("ExternalUserState");
                 var snapshotDateField = new DataField<DateTime>("SnapshotDate");
-                var schema = new ParquetSchema(idField, mailField, snapshotDateField);
+
+                var schema = new ParquetSchema(idField, mailField, upnField, displayNameField, userTypeField, accountEnabledField, preferredLanguageField,
+                    createdDateTimeField, lastPasswordChangeField, externalUserStateField, snapshotDateField);
 
                 await using var parquetWriter = await ParquetWriter.CreateAsync(schema, blobStream, Globals.ParquetOptions);
 
@@ -37,7 +47,15 @@ namespace GCStats
                 int count = 0;
 
                 var idBuffer = new List<string>(Globals.RowGroupBatchSize);
-                var mailBuffer = new List<string>(Globals.RowGroupBatchSize);
+                var mailBuffer = new List<string?>(Globals.RowGroupBatchSize);
+                var upnBuffer = new List<string?>(Globals.RowGroupBatchSize);
+                var displayNameBuffer = new List<string?>(Globals.RowGroupBatchSize);
+                var userTypeBuffer = new List<string?>(Globals.RowGroupBatchSize);
+                var accountEnabledBuffer = new List<bool?>(Globals.RowGroupBatchSize);
+                var preferredLanguageBuffer = new List<string?>(Globals.RowGroupBatchSize);
+                var createdDateTimeBuffer = new List<DateTime?>(Globals.RowGroupBatchSize);
+                var lastPasswordChangeBuffer = new List<DateTime?>(Globals.RowGroupBatchSize);
+                var externalUserStateBuffer = new List<string?>(Globals.RowGroupBatchSize);
                 var snapshotDateBuffer = new List<DateTime>(Globals.RowGroupBatchSize);
 
                 async Task FlushBatchAsync()
@@ -49,10 +67,26 @@ namespace GCStats
 
                     await groupWriter.WriteAsync(idField, idBuffer);
                     await groupWriter.WriteAsync(mailField, mailBuffer);
+                    await groupWriter.WriteAsync(upnField, upnBuffer);
+                    await groupWriter.WriteAsync(displayNameField, displayNameBuffer);
+                    await groupWriter.WriteAsync(userTypeField, userTypeBuffer);
+                    await groupWriter.WriteAsync<bool>(accountEnabledField, accountEnabledBuffer.ToArray().AsMemory());
+                    await groupWriter.WriteAsync(preferredLanguageField, preferredLanguageBuffer);
+                    await groupWriter.WriteAsync<DateTime>(createdDateTimeField, createdDateTimeBuffer.ToArray().AsMemory());
+                    await groupWriter.WriteAsync<DateTime>(lastPasswordChangeField, lastPasswordChangeBuffer.ToArray().AsMemory());
+                    await groupWriter.WriteAsync(externalUserStateField, externalUserStateBuffer);
                     await groupWriter.WriteAsync<DateTime>(snapshotDateField, snapshotDateBuffer.ToArray().AsMemory());
 
                     idBuffer.Clear();
                     mailBuffer.Clear();
+                    upnBuffer.Clear();
+                    displayNameBuffer.Clear();
+                    userTypeBuffer.Clear();
+                    accountEnabledBuffer.Clear();
+                    preferredLanguageBuffer.Clear();
+                    createdDateTimeBuffer.Clear();
+                    lastPasswordChangeBuffer.Clear();
+                    externalUserStateBuffer.Clear();
                     snapshotDateBuffer.Clear();
                 }
 
@@ -60,7 +94,18 @@ namespace GCStats
                 {
                     requestConfiguration.Headers.Add("ConsistencyLevel", "eventual");
                     requestConfiguration.QueryParameters.Top = 999;
-                    requestConfiguration.QueryParameters.Select = Users.UserQuerySelectParams;
+                    requestConfiguration.QueryParameters.Select = [
+                        "Id", 
+                        "Mail", 
+                        "UserPrincipalName", 
+                        "DisplayName",
+                        "AccountEnabled", 
+                        "CreatedDateTime", 
+                        "UserType",
+                        "PreferredLanguage",
+                        "LastPasswordChangeDateTime",
+                        "ExternalUserState"
+                    ];
                 });
 
                 var exceptionUsersArray = Auth.GetAppSetting("exceptionUsersArray", log, config);
@@ -74,7 +119,15 @@ namespace GCStats
                             if (user.Id != null && !exceptionUsersArray.Contains(user.Id))
                             {
                                 idBuffer.Add(user.Id);
-                                mailBuffer.Add(user.Mail ?? string.Empty);
+                                mailBuffer.Add(user.Mail);
+                                upnBuffer.Add(user.UserPrincipalName);
+                                displayNameBuffer.Add(user.DisplayName);
+                                userTypeBuffer.Add(user.UserType);
+                                accountEnabledBuffer.Add(user.AccountEnabled);
+                                preferredLanguageBuffer.Add(user.PreferredLanguage);
+                                createdDateTimeBuffer.Add(user.CreatedDateTime?.UtcDateTime);
+                                lastPasswordChangeBuffer.Add(user.LastPasswordChangeDateTime?.UtcDateTime);
+                                externalUserStateBuffer.Add(user.ExternalUserState);
                                 snapshotDateBuffer.Add(snapshotDate);
                                 count++;
 
